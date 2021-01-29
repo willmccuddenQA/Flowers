f63435f524bbb4be39d0a0d42240c4837280beb8
Will McCudden <wmccudden-hughes@qa.com>
2021-01-29 12:48:56 +0000

    fourth commit

--- FlowerController.js
+++ FlowerController.js
@@ -5,38 +5,45 @@ const createHeight = document.querySelector("#createHeight");
 const createPoisonous = document.querySelector("#createPoisonous");
 const createPrice = document.querySelector("#createPrice");
 const createType = document.querySelector("#createType");
+const flowerz = document.querySelector("#flowerz");
+
+const deleteID = document.querySelector("deleteInput");
 
 
 
-const printFlowerToScreen = (username) => {
+const printFlowerToScreen = (flowers) => {
     let user = document.createElement("p"); // <p> </p>
-    let text = document.createTextNode(`${username}`); // username
+    let text = document.createTextNode(` Colour: ${flowers.colour}, Height: ${flowers.height}, Poisonous: ${flowers.poisonous}, Price: ${flowers.price}, Type: ${flowers.type}`); // username
     user.appendChild(text); // <p> username </p>
-    peeps.appendChild(user);
+    flowerz.appendChild(user);
 }
 
 const retrieveData = () => {
-    fetch("http://localhost:8082/swagger-ui.html")
-    .then((response) => {
-        // check that the response is OK (i.e. 200)
-        if(response.status !== 200){
-            throw new Error("I don't have a status of 200");
-        }else{
-            console.log(response);
-            console.log(`response is OK (200)`);
-            //json-ify it (which returns a promise)
-            response.json().then((infofromserver) =>{
-                console.log(infofromserver);
-                console.log(infofromserver.data); // key - return array(6)
-                for(let flowers of infofromserver.data){
-                    console.log(flowers.colour);
-                    printNameToScreen(flowers.colour);
-                }
-            })
-        }
-    }).catch((err) => {
-        console.error(err);
-    })
+    fetch("http://localhost:8082/flower/read")
+        .then((response) => {
+            // check that the response is OK (i.e. 200)
+            if (response.status !== 200) {
+                throw new Error("I don't have a status of 200");
+            } else {
+                console.log(response);
+                console.log(`response is OK (200)`);
+                //json-ify it (which returns a promise)
+                response.json().then((infofromserver) => {
+                    console.log(infofromserver);
+                    //console.log(infofromserver.data); // key - return array(6)
+                    for (let flowers of infofromserver) {
+                        console.log(flowers);
+                        printFlowerToScreen(flowers);
+                    }
+                })
+            }
+        }).catch((err) => {
+            console.error(err);
+        })
+}
+
+const deleteFlowers = () => {
+    const flowerID = deleteID.value;
 }
 
 const createFlowers = () => {
@@ -46,7 +53,6 @@ const createFlowers = () => {
     const poisonousValue = createPoisonous.value;
     const priceValue = createPrice.value;
     const typeValue = createType.value;
-    console.log(poisonousValue);
 
     let data = {
         colour: colourValue,
@@ -58,17 +64,17 @@ const createFlowers = () => {
 
     console.log(data);
 
-    fetch("http://localhost:8082/flower/create",{
+    fetch("http://localhost:8082/flower/create", {
         method: "POST",
         body: JSON.stringify(data),
-        headers:{
-            "Content-Type":"application/json",
-            "Access-Control-Allow-Origin":"*"
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
         }
     })
-    .then(response=>response.json())
-    .then(info =>{
-        console.log(info);
-    })
-    .catch(err => console.error('ERROR!' + err));
+        .then(response => response.json())
+        .then(info => {
+            console.log(info);
+        })
+        .catch(err => console.error('ERROR!' + err));
 }
